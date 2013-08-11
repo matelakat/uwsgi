ddc9876c098352a54846f83255a223a58e1bb03a
Mate Lakat <mlakat@mlakat.hu>
2013-08-11 11:13:01 +0100

    consult python-config script for include libraries
    Amend gevent plugin as well

--- plugins/gevent/uwsgiplugin.py
+++ plugins/gevent/uwsgiplugin.py
@@ -1,7 +1,20 @@
+import os
 from distutils import sysconfig
+import subprocess
+
+def splitted_output_of(cmdline):
+    process = subprocess.Popen(cmdline.split(), stdout=subprocess.PIPE)
+    if process.wait():
+        raise Exception("Command %s returned non-zero" % cmdline)
+    return process.stdout.read().strip().split()
 
 NAME='gevent'
-CFLAGS = ['-I' + sysconfig.get_python_inc(), '-I' + sysconfig.get_python_inc(plat_specific=True)]
+
+python_config_script = os.environ.get('UWSGICONFIG_PYTHONCONFIG')
+if python_config_script:
+    CFLAGS = splitted_output_of(python_config_script + " --includes")
+else:
+    CFLAGS = ['-I' + sysconfig.get_python_inc(), '-I' + sysconfig.get_python_inc(plat_specific=True)]
 LDFLAGS = []
 LIBS = []
 
